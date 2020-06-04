2ca52920001bf21d7315b33186c437200df713b0
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-06-04 23:55:55 +0100

    All items listed

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,3 @@
 run migrate in sqlite: npm run knex-migrate
+run seeds: npm run knex-seed
 start server: npm run start-dev
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import knex from './database/connection';
 
 const routes = Router();
 
-routes.get('/users', (req, res) => {
-  res.json({ ok: 'test' });
+interface ItemData {
+  title: string;
+  image: string;
+  id: number;
+}
+
+routes.get('/items', async (req, res) => {
+  const items: ItemData[] = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return res.json(serializedItems);
 });
 
 export default routes;
